1e4f4eda6696ab44a596efcb8ee5f5b002506715
sachinzine <35653832+sachinzine@users.noreply.github.com>
2018-01-21 17:53:39 +0530

    Revert "Html Pages added"
    This reverts commit 326f47c7fa0d7b4ada274fffbf5cd6dd898a6f37.

--- HTML Pages/HM.js
+++
@@ -1,9 +0,0 @@
-<script>
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-</script>
